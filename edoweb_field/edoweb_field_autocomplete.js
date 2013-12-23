223d6afa798f308cb25654fd7bfff94f8e39477a
Felix Ostrowski <felix.ostrowski@googlemail.com>
2013-12-23 12:06:58 +0100

    Implement single search input autocomplete widget

--- edoweb_field/edoweb_field_autocomplete.js
+++ edoweb_field/edoweb_field_autocomplete.js
@@ -21,80 +21,25 @@
 
   Drupal.behaviors.edoweb_field = {
     attach: function (context, settings) {
-      $('body', context).delegate('a.autocomplete-remove', 'click', function(e) {
-        console.log("clicked");
-        $(this).closest('tr').hide();
-        $(this).closest('td').children('input')[0].value = "";
-        return false;
+      window.location.hash = 'focus';
+      $('.edoweb_autocomplete_widget').each(function(index, element) {
+        var field_name = $(this).attr('class').split(/\s+/)[1];
+        $(this).autocomplete({
+          source: Drupal.settings.basePath + 'edoweb/autocomplete/' + field_name,
+          minLength: 2,
+          select: function(event, ui) {
+            var value_input = $('input[name="edoweb_autocomplete_widget[' + field_name + '][value]"]');
+            var add_button = $('input[name="edoweb_autocomplete_widget_add_' + field_name + '"]');
+            if (!add_button.is(':disabled')) {
+              value_input.val(ui.item.value);
+              add_button.trigger('click');
+            } else {
+              value_input.val('');
+            }
+            return false;
+          }
+        });
       });
-    }
-  };
-
-
-  /**
-   * Puts the currently highlighted suggestion into the autocomplete field.
-   */
-  Drupal.jsAC.prototype.select = function (node) {
-    // Selection is handeled by Drupal.jsAC.prototype.hidePopup
-  };
-
-  /**
-   * Hides the autocomplete suggestions.
-   */
-  Drupal.jsAC.prototype.hidePopup = function (keycode) {
-    // Select item if the right key or mousebutton was pressed.
-    if (this.selected && ((keycode && keycode != 46 && keycode != 8 && keycode != 27) || !keycode)) {
-      this.input.value = $(this.selected).data('autocompleteId');
-      this.input.type = 'hidden';
-      var label = $('<input type="text" class="form-text autocomplete-selected" size="60" maxlength="128" disabled value="' + $(this.selected).data('autocompleteValue') + '" />');
-      var remove = $('<div class="form-item form-type-item"><a href="#" class="autocomplete-remove">[remove]</a></div>');
-      $(this.input).after(label);
-      $(this.input).parent().after($(this.input));
-      $(this.input).before(remove);
-    }
-    // Hide popup.
-    var popup = this.popup;
-    if (popup) {
-      this.popup = null;
-      $(popup).fadeOut('fast', function () { $(popup).remove(); });
-    }
-    this.selected = false;
-    $(this.ariaLive).empty();
-  };
-
-  /**
-   * Fills the suggestion popup with any matches received.
-   */
-  Drupal.jsAC.prototype.found = function (matches) {
-    // If no value in the textfield, do not show the popup.
-    if (!this.input.value.length) {
-      return false;
-    }
-
-    // Prepare matches.
-    var ul = $('<ul></ul>');
-    var ac = this;
-    for (key in matches) {
-      $('<li></li>')
-        .html($('<div></div>').html(matches[key]))
-        .mousedown(function () { ac.select(this); })
-        .mouseover(function () { ac.highlight(this); })
-        .mouseout(function () { ac.unhighlight(this); })
-        .data('autocompleteId', key)
-        .data('autocompleteValue', matches[key])
-        .appendTo(ul);
-    }
-
-    // Show popup with matches, if any.
-    if (this.popup) {
-      if (ul.children().length) {
-        $(this.popup).empty().append(ul).show();
-        $(this.ariaLive).html(Drupal.t('Autocomplete popup'));
-      }
-      else {
-        $(this.popup).css({ visibility: 'hidden' });
-        this.hidePopup();
-      }
     }
   };
 
