0990c3c19dcecd56f4905a8125636d0b1c24e892
Felix Ostrowski <felix.ostrowski@googlemail.com>
2014-05-08 10:43:47 +0200

    Rewrite of linked data fields
    - LD-ref results as table
    - use overlay for lookups
    - display values as AJAX table
    - fix interference with system "Add more"-button
      - Use #tree array with #parents set, #default_value and #name to
        property let drupal detect the triggering element name
      - Prevent from behaviours being attached twice

--- edoweb_field/edoweb_field_reference.js
+++ edoweb_field/edoweb_field_reference.js
@@ -28,8 +28,19 @@
     })(jQuery);
   }
 
+  /**
+   * Function returns an entities label.
+   */
+  function entity_list(entity_type, entity_curies) {
+    return $.get(Drupal.settings.basePath + 'edoweb_entity_list/' + entity_type + '?' + $.param({ids: entity_curies}));
+  }
+
+  var attached = false;
   Drupal.behaviors.edoweb_field_reference = {
     attach: function (context, settings) {
+      if (attached) return;
+      attached = true;
+      // Load entity into fieldset
       $(context).find('fieldset.edoweb_ld_reference').find('a.fieldset-title').each(function(i, element) {
         var link = $(element).closest('fieldset').children('div.fieldset-wrapper').children('input[type=hidden]').get(0);
         if (link) {
@@ -57,7 +68,9 @@
           };
         }
       });
-      $(context).find('*[data-curie]').each(function() {
+
+      // Load labels for Linked Data References
+      $(context).find('a[data-curie]').each(function() {
         var link = $(this);
         entity_label('edoweb_basic', link.attr('data-curie')).onload = function() {
           if (this.status == 200) {
@@ -65,8 +78,125 @@
           }
         };
       });
+
+      // Attach lookup overlay to form
+      var lookup_overlay = $('<div />').dialog({
+        autoOpen: false,
+        //resizable: false,
+        width: 'auto'
+      });
+
+      $(context).find('.field-type-edoweb-ld-reference').each(function() {
+        var source = $(this);
+        // Hide input elements
+        source.find('input.edoweb_autocomplete_widget').hide();
+        source.find('input[type="submit"]').hide();
+        // Hide value elements, display AJAX entity list
+        var curies = [];
+        source.find('input[data-curie]').each(function() {
+          var input = $(this);
+          var curie = input.attr('data-curie');
+          input.hide();
+          if (curie != '') curies.push(curie);
+        });
+
+        if (curies.length > 0) {
+          var throbber = $('<div class="ajax-progress"><div class="throbber">&nbsp;</div></div>')
+          $(source).append(throbber);
+          entity_list('edoweb_basic', curies).onload = function () {
+            if (this.status == 200) {
+              var result_table = $(this.responseText);
+              result_table.find('tbody > tr').each(function() {
+                var row = $(this);
+                $(this).children('td').last()
+                  .append('<button>Entfernen</button>')
+                  .bind('click', function(event) {
+                    source.find('input[data-curie="' + row.attr('data-curie') + '"]').val('');
+                    if (row.next().length == 0 && row.prev().length == 0) {
+                      row.closest('table').remove();
+                    } else {
+                      row.remove();
+                    }
+                    return false;
+                  });
+              });
+              source.append(result_table);
+              throbber.remove();
+            }
+          };
+        }
+
+        // Open lookup overlay
+        var button = $('<a href="#"> [+]</a>').click(function(e) {
+          lookup_overlay.html('<div />');
+          refreshTable(lookup_overlay, source);
+          lookup_overlay.dialog('open');
+          return false;
+        });
+        source.find('label').append(button);
+      });
     }
   };
+
+function refreshTable(container, source, page, sort, order, term) {
+  if(!page) page = 0;
+  if(!sort) sort = '';
+  if(!order) order = '';
+  if(!term) term = '';
+
+  var bundle_name = source.find('input.edoweb_autocomplete_widget').attr('data-bundle');
+  var field_name = source.find('input.edoweb_autocomplete_widget').attr('data-field');
+  var qurl = Drupal.settings.basePath + '?q=edoweb/search/' + bundle_name + '/' + field_name;
+
+  jQuery.ajax({
+    cache: false,
+    url: qurl,
+    data: {page: page, sort: sort, order: order, 'query[0][term]': term},
+    dataType: 'text',
+    error: function(request, status, error) {
+      alert(status);
+    },
+    success: function(data, status, request) {
+      var html = data;
+
+      container.html(html);
+
+      container.find('th a')
+        .add(container.find('.pager-item a'))
+        .add(container.find('.pager-first a'))
+        .add(container.find('.pager-previous a'))
+        .add(container.find('.pager-next a'))
+        .add(container.find('.pager-last a'))
+          .click(function(el, a, b, c) {
+            var url = jQuery.url(el.currentTarget.getAttribute('href'));
+            refreshTable(container, source, url.param('page'), url.param('sort'), url.param('order'), url.param('query[0][term]'));
+            return (false);
+          });
+
+      container.find('input[name="op"]').click(function() {
+        var term = container.find('input[type="text"]').val();
+        refreshTable(container, source, null, null, null, term);
+        return false;
+      });
+
+      container.find('.sticky-enabled > tbody > tr').each(function() {
+        var row = jQuery(this);
+        jQuery(this).children('td').last()
+          .append('<button>Hinzufügen</button>')
+          .bind('click', function(event) {
+            var resource_uri = row.children('td').first().children('a').first().text();
+            source.find('input.edoweb_autocomplete_widget').val(resource_uri);
+            source.find('input[type="submit"]').click();
+            container.dialog('close');
+            return false;
+          });
+      });
+
+      Drupal.attachBehaviors(container);
+
+    }
+  });
+}
 
 })(jQuery);
 
